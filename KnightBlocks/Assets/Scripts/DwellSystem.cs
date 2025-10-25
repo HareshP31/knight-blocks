e407b4c2af98b81a8cafb2d0282b758e26ff0966
ChrisTuck04 <christopher@tuckman.us>
2025-10-25 09:49:18 -0400

    menu ui created with dwell system implementation

---
+++ KnightBlocks/Assets/Scripts/DwellSystem.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DwellSystem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Tooltip("How long to 'hover' before triggering the event.")]
+    public float dwellTime = 1.5f;
+
+    [Tooltip("A grace period after activation where no dwell buttons can be triggered.")]
+    public float activationCooldown = 1f;
+
+    [Tooltip("This event will be fired after the dwell time.")]
+    public UnityEvent onDwell;
+    public static bool isAnyButtonCoolingDown = false;
+
+    private bool isPointerOver = false;
+    private float dwellTimer = 0f;
+    private bool isTimerRunning = false;
+
+    public static MonoBehaviour coroutineRunner;
+
+    private void Update()
+    {
+        if (isPointerOver)
+        {
+            if (!isTimerRunning)
+            {
+                if (!isAnyButtonCoolingDown)
+                {
+                    isTimerRunning = true;
+                    dwellTimer = 0f;
+                }
+            }
+            else
+            {
+                dwellTimer += Time.deltaTime;
+
+                if (dwellTimer >= dwellTime)
+                {
+                    TriggerDwell();
+                }
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerOver = false;
+        isTimerRunning = false;
+        dwellTimer = 0f;
+    }
+
+    private void TriggerDwell()
+    {
+        Debug.Log("Dwell complete on " + gameObject.name);
+        isTimerRunning = false;
+        dwellTimer = 0f;
+
+        onDwell.Invoke();
+
+        if (coroutineRunner != null)
+        {
+            coroutineRunner.StartCoroutine(GlobalCooldown(activationCooldown));
+        }
+    }
+
+    private static IEnumerator GlobalCooldown(float cooldownTime)
+    {
+        Debug.Log("Starting global cooldown for dwell buttons.");
+        isAnyButtonCoolingDown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        isAnyButtonCoolingDown = false;
+        Debug.Log("Global cooldown for dwell buttons ended.");
+    }
+    
+    private void OnDisable()
+    {
+        isPointerOver = false;
+        isTimerRunning = false;
+        dwellTimer = 0f;
+    }
+}
